f2367464a60c39ed1f02053faa98c2b57a842ab9
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-05-18 18:31:56 +0100

    Logger now returns error message, which may be used in the frontend

--- src/api/logger.js
+++ src/api/logger.js
@@ -1,3 +1,12 @@
 export function validationError(request, h, error) {
   console.log(error.message);
+  return h
+    .response({
+      success: false,
+      statusCode: 400,
+      error: "Bad Request",
+      message: error.message,
+    })
+    .code(400)
+    .takeover();
 }
